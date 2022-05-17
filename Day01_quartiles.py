843cfcdc72d393e7d0067d272892c1671c357a6c
Pall Pandiyan <pallpandiyan@gmail.com>
2022-05-17 16:48:32 +0530

    solve, but hackerrank does not accept it

---
+++ Day01_quartiles.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'quartiles' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+
+def median(arr):
+    # the array should be sorted before sending
+    n = len(arr)
+    mid = n//2
+    if (n % 2 == 1):
+        return arr[mid]
+    else:
+        return (arr[mid-1]+arr[mid])//2
+
+
+def quartiles(arr):
+    # Write your code here
+    arr.sort()
+    n = len(arr)
+    mid = n//2
+    result = []
+
+    if(n == 3):
+        return arr
+
+    if(n % 2 == 1):
+        result.append(median(arr[:mid]))
+        result.append(arr[mid])
+        result.append(median(arr[mid+1:]))
+    else:
+        result.append(median(arr[:mid-1]))
+        result.append((arr[mid-1]+arr[mid])//2)
+        result.append(median(arr[mid+1:]))
+
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    data = list(map(int, input().rstrip().split()))
+
+    res = quartiles(data)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
